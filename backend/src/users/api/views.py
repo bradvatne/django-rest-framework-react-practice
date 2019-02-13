243d4b62bf69889fcf358cabf0e88c7ef2750b9f
notbrad <45443057+notbrad@users.noreply.github.com>
2019-02-12 16:48:10 -0800

    Initial commit

---
+++ backend/src/users/api/views.py
@@ -0,0 +1,11 @@
+from users.models import User
+from .serializers import UserSerializer
+from rest_framework import viewsets
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+    
